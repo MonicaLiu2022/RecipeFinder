7b35f4fa34a60352657c589e85af8029c1e01cb7
Fengying Liu <bgj205@myy.haaga-helia.fi>
2022-09-20 00:09:43 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, View, FlatList, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => response.json())
+      .then(recipes => setRecipes(recipes.meals))
+      .catch(error => {
+        Alert.alert('Error', error);
+      });
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={recipes}
+        renderItem={({ item }) =>
+          <View>
+            <Text
+              style={{ fontSize: 18, fontWeight: "bold" }}>
+              {item.strMeal}
+            </Text>
+            <Image
+              style={{ width: 60, height: 60 }}
+              source={{ uri: item.strMealThumb }} />
+          </View>
+        }
+        ItemSeparatorComponent={listSeparator} />
+      <TextInput
+        style={{ fontSize: 18, width: 200 }}
+        placeholder='keyword'
+        onChangeText={text => setKeyword(text)}
+      />
+      <Button
+        title="Find"
+        onPress={getRecipes}
+      />
     </View>
   );
 }
@@ -13,8 +61,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 40,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginLeft: 20,
     justifyContent: 'center',
   },
 });
